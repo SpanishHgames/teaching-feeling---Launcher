d34c774f40fc247645755c12a2e81d84edc27186
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-07-02 12:15:58 -0600

    Create version.js

---
+++ src/js/version.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const replaceText = (selector, text) => {
+        const element = document.getElementById(selector);
+        if (element) element.innerText = text;
+    };
+
+    replaceText('launcher-version', '1.0.0');
+});
