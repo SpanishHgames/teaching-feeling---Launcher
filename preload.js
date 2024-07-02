9c5af5b59285abf8144c447230b30c7bf32e821a
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-07-01 22:46:39 -0600

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,9 +1,19 @@
-// preload.js
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  openFileDialog: () => ipcRenderer.invoke('open-file-dialog'),
+  saveConfig: () => ipcRenderer.invoke('save-config'),
+  startGame: () => ipcRenderer.invoke('start-game'),
+  getExecutablePath: () => ipcRenderer.invoke('get-executable-path'),
+});
+
+
+
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector);
     if (element) element.innerText = text;
   };
 
-  replaceText(`launcher-version`, '1.0.0');
+  replaceText(launcher-version, '1.0.0');
 });
