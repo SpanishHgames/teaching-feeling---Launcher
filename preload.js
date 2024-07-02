88726a2e717059e2a49af70ee538e5bdc10db0fc
twib <107765373+StaffV77@users.noreply.github.com>
2024-07-01 20:32:12 -0600

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,12 +1,9 @@
 // preload.js
 window.addEventListener('DOMContentLoaded', () => {
-    const replaceText = (selector, text) => {
-      const element = document.getElementById(selector);
-      if (element) element.innerText = text;
-    };
-  
-    for (const type of ['chrome', 'node', 'electron']) {
-      replaceText(`${type}-version`, process.versions[type]);
-    }
-  });
-  
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector);
+    if (element) element.innerText = text;
+  };
+
+  replaceText(`launcher-version`, '1.0.0');
+});
