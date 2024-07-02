8dd2a1742927745cd2b0e23839cd0b8ec6d83d3d
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-07-02 12:15:34 -0600

    Update preload.js

--- preload.js
+++ preload.js
@@ -6,14 +6,3 @@ contextBridge.exposeInMainWorld('electronAPI', {
   startGame: () => ipcRenderer.invoke('start-game'),
   getExecutablePath: () => ipcRenderer.invoke('get-executable-path'),
 });
-
-
-
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector);
-    if (element) element.innerText = text;
-  };
-
-  replaceText(launcher-version, '1.0.0');
-});
