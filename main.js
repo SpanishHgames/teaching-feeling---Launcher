9246d5f9732cac381068c4284311cbf9f4393935
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-07-03 13:02:22 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const path = require('path');
 const fs = require('fs');
 const { exec } = require('child_process');
 
-let executablePath = null;
+let executablePathEs = null;
+let executablePathEn = null;
 let originalContent = null;
 let mainWindow = null;
 
@@ -19,19 +20,27 @@ function createWindow() {
     },
   });
 
-  mainWindow.loadFile('src/html/main.html');
+  mainWindow.loadFile(path.join(__dirname, 'src', 'html', 'main.html'));
   mainWindow.setMenu(null);
 
-  // Load the configuration file when the window is created
-  loadConfig();
+  // Cargar las configuraciones cuando se crea la ventana
+  loadConfigs();
 }
 
-function loadConfig() {
-  const configPath = path.join(app.getPath('userData'), 'config.json');
-  if (fs.existsSync(configPath)) {
-    const fileData = fs.readFileSync(configPath);
-    const config = JSON.parse(fileData);
-    executablePath = config.executablePath;
+function loadConfigs() {
+  const configPathEs = path.join(app.getPath('userData'), 'configEs.json');
+  const configPathEn = path.join(app.getPath('userData'), 'configEn.json');
+
+  if (fs.existsSync(configPathEs)) {
+    const fileDataEs = fs.readFileSync(configPathEs);
+    const configEs = JSON.parse(fileDataEs);
+    executablePathEs = configEs.executablePath;
+  }
+
+  if (fs.existsSync(configPathEn)) {
+    const fileDataEn = fs.readFileSync(configPathEn);
+    const configEn = JSON.parse(fileDataEn);
+    executablePathEn = configEn.executablePath;
   }
 }
 
@@ -49,7 +58,21 @@ app.on('activate', () => {
   }
 });
 
-ipcMain.handle('open-file-dialog', async () => {
+ipcMain.handle('open-file-dialog-es', async () => {
+  const result = await dialog.showOpenDialog({
+    properties: ['openFile'],
+    filters: [{ name: 'Ejecutables', extensions: ['exe'] }],
+  });
+
+  if (result.canceled) {
+    return null;
+  } else {
+    executablePathEs = result.filePaths[0];
+    return executablePathEs;
+  }
+});
+
+ipcMain.handle('open-file-dialog-en', async () => {
   const result = await dialog.showOpenDialog({
     properties: ['openFile'],
     filters: [{ name: 'Executables', extensions: ['exe'] }],
@@ -58,63 +81,85 @@ ipcMain.handle('open-file-dialog', async () => {
   if (result.canceled) {
     return null;
   } else {
-    executablePath = result.filePaths[0];
-    return executablePath;
+    executablePathEn = result.filePaths[0];
+    return executablePathEn;
   }
 });
 
-ipcMain.handle('save-config', async () => {
-  if (executablePath) {
-    const configPath = path.join(app.getPath('userData'), 'config.json');
-    const fileData = { executablePath };
-    fs.writeFileSync(configPath, JSON.stringify(fileData, null, 2));
-    return 'Configuration saved successfully.';
+ipcMain.handle('save-config-es', async () => {
+  if (executablePathEs) {
+    const configPathEs = path.join(app.getPath('userData'), 'configEs.json');
+    const fileDataEs = { executablePath: executablePathEs };
+    fs.writeFileSync(configPathEs, JSON.stringify(fileDataEs, null, 2));
+    return 'Configuración guardada exitosamente para la aplicación en español.';
   }
-  return 'No executable selected.';
+  return 'No se ha seleccionado ningún ejecutable para la aplicación en español.';
 });
 
-ipcMain.handle('start-game', () => {
+ipcMain.handle('save-config-en', async () => {
+  if (executablePathEn) {
+    const configPathEn = path.join(app.getPath('userData'), 'configEn.json');
+    const fileDataEn = { executablePath: executablePathEn };
+    fs.writeFileSync(configPathEn, JSON.stringify(fileDataEn, null, 2));
+    return 'Configuration saved successfully for the application in English.';
+  }
+  return 'No executable selected for the application in English.';
+});
+
+ipcMain.handle('start-game-es', () => {
+  return startGame(executablePathEs, 'español');
+});
+
+ipcMain.handle('start-game-en', () => {
+  return startGame(executablePathEn, 'English');
+});
+
+function startGame(executablePath, language) {
   return new Promise((resolve, reject) => {
     if (executablePath) {
       const indexPath = path.join(path.dirname(executablePath), 'resources', 'app', 'index.html');
       if (fs.existsSync(indexPath)) {
         originalContent = fs.readFileSync(indexPath, 'utf8');
         const modifiedContent = originalContent.replace('</body>', `
-          <div style="position: fixed; top: 20px; left: 20px;"><div style="color: rgba(165, 161, 161, 0.6); font-family: Arial, sans-serif; font-size: 24px; font-weight: bold; text-align: center; display: inline;">TeachFeel v1.0.0</div></div>
+          <div style="position: fixed; top: 20px; left: 20px;"><div style="color: rgba(165, 161, 161, 0.6); font-family: Arial, sans-serif; font-size: 24px; font-weight: bold; text-align: center; display: inline;">TeachFeel v1.0.0 (${language})</div></div>
   </body>
         `);
         fs.writeFileSync(indexPath, modifiedContent);
 
-        // Hide the main window before starting the game
+        // Ocultar la ventana principal antes de iniciar el juego
         mainWindow.hide();
 
         exec(`"${executablePath}"`, (error, stdout, stderr) => {
-          // Show the main window after the game closes
+          // Mostrar la ventana principal después de cerrar el juego
           mainWindow.show();
 
-          // Revert changes to index.html after the game closes
+          // Revertir los cambios en index.html después de cerrar el juego
           fs.writeFileSync(indexPath, originalContent);
 
           if (error) {
-            console.error(`Error executing file: ${error.message}`);
-            resolve('Failed to start the game.');
+            console.error(`Error al ejecutar el archivo: ${error.message}`);
+            resolve(`Error al iniciar el juego en ${language}.`);
           } else if (stderr) {
             console.error(`Error: ${stderr}`);
-            resolve('Failed to start the game.');
+            resolve(`Error al iniciar el juego en ${language}.`);
           } else {
-            console.log(`Output: ${stdout}`);
-            resolve('Game started successfully.');
+            console.log(`Salida: ${stdout}`);
+            resolve(`Juego iniciado exitosamente en ${language}.`);
           }
         });
       } else {
-        resolve('Index file not found.');
+        resolve(`Archivo index no encontrado para la aplicación en ${language}.`);
       }
     } else {
-      resolve('Executable path not set.');
+      resolve(`Ruta del ejecutable no establecida para la aplicación en ${language}.`);
     }
   });
+}
+
+ipcMain.handle('get-executable-path-es', () => {
+  return executablePathEs;
 });
 
-ipcMain.handle('get-executable-path', () => {
-  return executablePath;
+ipcMain.handle('get-executable-path-en', () => {
+  return executablePathEn;
 });
